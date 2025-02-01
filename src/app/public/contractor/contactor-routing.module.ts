0b38866960fbab4538377466626ab80010906637
volocarucatalin <volocaru.catalin17@gmail.com>
2025-02-01 11:12:36 +0000

    Creating login function whit predefined variables

---
+++ src/app/public/contractor/contactor-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ContractorComponent} from './contractor.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: ContractorComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ContactorRoutingModule {
+
+}
